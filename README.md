57442bc9d81760c3fcca2870153fcdb16a12ccf0
Matt Meleen <mmeleen@utexas.edu>
2020-08-28 03:51:15 -0500

    Updated files

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Task is to build a CMS solution for managing a company's employees using node, i
 
 
 ![Database Schema](Assets/schema.png)
+
+![App Demo] (Assets/EmpTrackMySQLDemo.gif)
   
-Build a command-line application that at a minimum allows the user to:
+Build a command-line application that allows the user to:
 
   * Add departments, roles, employees
 
@@ -19,17 +21,3 @@ As a business owner
 I want to be able to view and manage the departments, roles, and employees in my company
 So that I can organize and plan my business
 ```
-
-## Criteria
-
-* Functional application.
-
-* GitHub repository with a unique name and a README describing the project.
-
-* The command-line application should allow users to:
-
-  * Add departments, roles, employees
-
-  * View departments, roles, employees
-
-  * Update employee roles, managers
--- seed.sql
+++ seed.sql
@@ -42,59 +42,8 @@ INSERT INTO department (name) values
   ('research'),
   ('sales');
 
-SELECT * FROM employee;
+SELECT * FROM employees;
 SELECT * FROM roles;
 SELECT * FROM department;
-
--- DROP DATABASE IF EXISTS employee_db;
--- CREATE DATABASE employee_db;
--- USE employee_db;
-
--- CREATE TABLE employee(
---   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---   first_name VARCHAR(30),
---   last_name VARCHAR(30),
---   role_id INTEGER,
---   --role_id INTEGER FOREIGN KEY REFERENCES roles(id),
---   CONSTRAINT fk_role
---     FOREIGN KEY (role_id) 
---         REFERENCES roles(id),
---   manager_id INTEGER
---   --manager_id INTEGER FOREIGN KEY REFERENCES ????
--- );
-
--- CREATE TABLE roles(
---   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---   title VARCHAR(30),
---   salary DECIMAL(10,2),
---   --department_id INTEGER
---   department_id INTEGER FOREIGN KEY REFERENCES department.id
--- );
-
--- CREATE TABLE department(
---   id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
---   name VARCHAR(30)
--- );
-
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('Jane', 'Austen', 1);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('Mark', 'Twain', 2);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('Lewis', 'Carroll', 3);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('Robert', 'Frost', 4);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('J.K.', 'Rowling', 1);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('Kurt', 'Vonnegut', 2);
--- INSERT INTO employees (first_name, last_name, role_id, manager_id) values ('George', 'Orwell', 3);
-
--- INSERT INTO roles (title, salary, department_id) values ('manager', 120000,);
--- INSERT INTO roles (title, salary, department_id) values ('engineer', 80000);
--- --INSERT INTO roles (title, salary, department_id) values ('engineer', );
--- INSERT INTO roles (title, salary, department_id) values ('associate', 50000);
--- --INSERT INTO roles (title, salary, department_id) values ('associate', 2);
--- INSERT INTO roles (title, salary, department_id) values ('intern', 0);
--- --INSERT INTO roles (title, salary, department_id) values ('intern', 0);
-
--- INSERT INTO department (name) values ('aerospace');
--- INSERT INTO department (name) values ('research');
--- INSERT INTO department (name) values ('customer service');
--- INSERT INTO department (name) values ('sales');
 
 
